1d3a41a68b7b22addadf703d0e25a4c5e053d974
iamAnugrahmk <anugrahmkmayyil@gmail.com>
2025-04-04 12:49:18 +0530

    fourth

--- clickerpr/clickerapp/admin.py
+++ clickerpr/clickerapp/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from .models import *
+from django.contrib.auth.models import Group, User
+from .models import Profile
 # Register your models here.
 
-admin.site.register()
+# unregiserd
+admin.site.unregister(Group)
+
+# mixprof info in2 usr info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+# xtend usr model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+# user fields
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+# unregi initial usr
+admin.site.unregister(User)
+# regi user&prfl
+admin.site.register(User, UserAdmin)
+# admin.site.register(Profile)
+
