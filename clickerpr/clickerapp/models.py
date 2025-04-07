0d7787ed2f767a374ee9033376300e9322d3c772
iamAnugrahmk <anugrahmkmayyil@gmail.com>
2025-04-07 12:44:57 +0530

    fifth

--- clickerpr/clickerapp/models.py
+++ clickerpr/clickerapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 # usr profile mdl
@@ -11,11 +12,12 @@ class Profile(models.Model):
                                      related_name="followed_by",
                                      symmetrical=False,
                                      blank=True)
-
+    date_modified = models.DateTimeField(User, auto_now=True)
     def __str__(self):
         return self.user.username
 
 # profile wn new user sinup
+# @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         user_profile = Profile(user=instance)
--- clickerpr/clickerapp/views.py
+++ clickerpr/clickerapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .models import Profile
 
 # Create your views here.
 def home(request):
@@ -6,4 +8,17 @@ def home(request):
 
 
 def profile_list(request):
-    return render(request, 'profile_list.html')
+    if request.user.is_authenticated:
+        profiles = Profile.objects.exclude(user =request.user) 
+        return render(request, 'profile_list.html',{"profiles": profiles})
+    else:
+        messages.success(request,("you must be logged.."))
+        return redirect('home')
+    
+def profile(request,pk):
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user_id=pk)
+        return render(request,"profile.html",{"profile":profile})
+    else:
+         messages.success(request,("you must be logged.."))
+         return redirect('home')
