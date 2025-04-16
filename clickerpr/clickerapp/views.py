2ab3c2775c2a0eaeecf1c1cbeac23138232eef10
iamAnugrahmk <anugrahmkmayyil@gmail.com>
2025-04-16 12:27:34 +0530

    sixth

--- clickerpr/clickerapp/views.py
+++ clickerpr/clickerapp/views.py
@@ -18,6 +18,7 @@ def profile_list(request):
 def profile(request,pk):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user_id=pk)
+        # 
         return render(request,"profile.html",{"profile":profile})
     else:
          messages.success(request,("you must be logged.."))
