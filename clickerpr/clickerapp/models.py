2ba3abd3bc4d9ed85af72deab36ce4104620f7a4
jasim-3 <jasimaachi333@gmail.com>
2025-04-21 11:02:04 +0530

    seventh

--- clickerpr/clickerapp/models.py
+++ clickerpr/clickerapp/models.py
@@ -3,6 +3,18 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 # Create your models here.
+# add clickmsg
+class Click(models.Model):
+    user = models.ForeignKey(
+        User, related_name="clicks",
+        on_delete=models.DO_NOTHING
+    )
+    body = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} ({self.created_at:%y-%m-%d %H:%M}): {self.body}..."
+
 
 # usr profile mdl
 
--- clickerpr/clickerapp/views.py
+++ clickerpr/clickerapp/views.py
@@ -18,7 +18,18 @@ def profile_list(request):
 def profile(request,pk):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user_id=pk)
-        # 
+        #post from logic
+        if request.method == "POST":
+            #get currentuser
+            current_user_profile = request.user.profile
+            action = request.POST['follow']
+            # follow or un follow
+            if action == "unfollow":
+                current_user_profile.follows.remove(profile)
+            elif action =="follow" :
+                current_user_profile.follows.add(profile)
+            # save currentprofl
+            current_user_profile.save()
         return render(request,"profile.html",{"profile":profile})
     else:
          messages.success(request,("you must be logged.."))
